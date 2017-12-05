3d9c6c1bc3e5cd7120af5b969609260146092869
Davide Bortolami <d@fermiumlabs.com>
2017-12-05 01:19:39 +0100

    Use SKU if mfg part number does not exist. Improved debug messages

--- octoerp/octo_erp/octo_erp.py
+++ octoerp/octo_erp/octo_erp.py
@@ -14,27 +14,40 @@ def octopart_lookup(sq_number):
 	sq = frappe.get_doc('Supplier Quotation', sq_number)
 	supplier = frappe.get_value('Supplier',sq.supplier,'octopart_seller_name') or sq.supplier
 	for item in sq.items:
-		mfg, mfg_pn = frappe.get_value('Item', item.item_code, ['manufacturer','manufacturer_part_no'])
+		mfg, mfg_pn, sku = frappe.get_value('Item', item.item_code, ['manufacturer','manufacturer_part_no', 'item_code'])
 		api_key = frappe.db.get_single_value('OctoERP Settings', "api_key")
+		#frappe.msgprint(_("{0}     {1}      {2}").format(mfg, mfg_pn, sku))
+
+		mpn_or_sku = ""
+		# Give precedence to the mfg_pn if existing
+		if mfg_pn:
+			mpn_or_sku = mfg_pn
+		else:
+			mpn_or_sku = sku
 
 		url = 'http://octopart.com/api/v3/parts/match?'
-		url += '&queries=[{{"mpn_or_sku":"{0}"}}]'.format(mfg_pn)
+		url += '&queries=[{{"mpn_or_sku":"{0}"}}]'.format(mpn_or_sku)
 		url += '&apikey={0}'.format(api_key)
-
+		
+		#frappe.msgprint(_("{0}").format(url))
+		
 		data = urllib.urlopen(url).read()
 		response = json.loads(data)
-
+		
+		#frappe.msgprint(_("{0}").format(response))
+		
 		found = 0
 
 		item.rate = 0
-		no_seller = 0
+		found_seller = False
 		if 'results' in response:
 			for result in response['results']:
 				for item_result in result['items']:
-					if item_result['manufacturer']['name'] == mfg:
+					# If we have a manufacturer (not mfg is false) only match the same manufacturer, otherwise all
+					if item_result['manufacturer']['name'] == mfg or not mfg:
 						for offer in item_result['offers']:
 							if offer['seller']['name'] == supplier:
-								no_seller = 1
+								found_seller = True
 								if sq.currency in offer['prices']:
 									for qty_rate in offer['prices'][sq.currency]:
 										if qty_rate[0] <= item.qty:
@@ -42,12 +55,14 @@ def octopart_lookup(sq_number):
 												item.rate = qty_rate[1]
 											elif qty_rate[1] < item.rate:
 												item.rate = qty_rate[1]
-		if no_seller == 0:
+
+		if not found_seller:
 			frappe.msgprint(_("{0} does not supply {1}. Please check that the supplier name matches the Octopart name list exactly").format(supplier, item.item_name))
 		elif item.rate > 0:
 			frappe.msgprint(_("Best rate that {0} supplies item {1} at is {2}").format(sq.supplier, item.item_code,item.rate))
 		else:
-			frappe.msgprint(_("Could not find in correct currency for {1}.").format(item.item_name))
+			frappe.msgprint(_("Could not find currency {0} for {1}.").format(sq.currency,item.item_name))
+
 	sq.save()
 
 @frappe.whitelist()
